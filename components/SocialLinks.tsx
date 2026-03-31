69baa2c0921ca99f90aae66d61ed72209cf9a819
joseechavarrimarin-ctrl <joseechavarrimarin@icloud.com>
2026-03-31 19:45:20 +0200

    Update Discord link in SocialLinks component

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,6 +1,6 @@
 const links = [
   { label: 'Twitter / X', href: 'https://twitter.com/YOUR_HANDLE' },
-  { label: 'Discord', href: 'https://discord.gg/fTW2ePTYk' },
+  { label: 'Discord', href: 'https://discord.gg/9sfzXrt6r' },
   { label: 'GitHub', href: 'https://github.com/YOUR_USERNAME/last-acre' },
 ]
 
